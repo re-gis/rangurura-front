35f259755bfb70ba4dbebcb42c6dc6963d5e0256
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-04-18 10:33:48 +0200

    feat: start loader

--- src/components/RedirectionLoader/index.tsx
+++ src/components/RedirectionLoader/index.tsx
@@ -1,9 +1,7 @@
-const RedirectionLoader = ()=>{
-    return(
-        <div className="w-screen h-screen absolute top-0 left-0 flex justify-center items-center">
-            
-        </div>
-    )
-}
+const RedirectionLoader = () => {
+  return (
+    <div className="w-screen h-screen absolute top-0 left-0 flex justify-center items-center"></div>
+  );
+};
 
 export default RedirectionLoader;
